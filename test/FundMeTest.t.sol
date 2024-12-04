abbd8841eb0b248283bdb73cd77e35a83ac9dd00
VahagnCh <vahagnchavushyan@gmail.com>
2024-12-04 15:44:47 -0600

    feat: new test OwnerIsMsgSender

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -9,11 +9,18 @@ contract FundMeTest is Test {
     FundMe fundMe;
 
     function setUp() external {
+        // us -> FundeMeTest -> FundMe
         fundMe = new FundMe();
     }
 
     function testMinimumDollarIsFive() public view {
         assertEq(fundMe.MINIMUN_USD(), 5e18);
+    }
+
+    function testOwnerIsMsgSender() public view {
+        console.log(fundMe.i_owner());
+        console.log(msg.sender);
+        assertEq(fundMe.i_owner(), address(this));
     }
 
 
